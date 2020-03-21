422b56ebd4ed960b383a44ec3e4aec2180bc7d74
crash2bash <sachagurov@gmail.com>
2020-03-21 08:35:58 +0200

    work on js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,20 @@
 'use strict';
 
-const overlay = document.querySelector('.overlay');
 const buyButton = document.querySelectorAll('.button--buy');
 const shoppingModal = document.querySelector('.shopping');
 const checkoutModal = document.querySelector('.checkout');
+const thanksModal = document.querySelector('.thanks');
+const overlay = document.querySelector('.overlay');
+const shoppingBag = document.querySelector('.header__basket-icon');
 const closeButton = document.querySelectorAll('.shopping__close');
 const checkoutButton = document.querySelector('.button--checkout');
+const paymentButton = document.querySelector('.button--payment');
+
+shoppingBag.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  overlay.classList.add('show');
+  shoppingModal.classList.add('show');
+});
 
 buyButton.forEach(button => {
   button.addEventListener('click', function() {
@@ -14,12 +23,34 @@ buyButton.forEach(button => {
   });
 });
 
+checkoutButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  shoppingModal.classList.remove('show');
+  checkoutModal.classList.add('show');
+});
+
+paymentButton.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  shoppingModal.classList.remove('show');
+  checkoutModal.classList.remove('show');
+  thanksModal.classList.add('show');
+});
+
 closeButton.forEach(button => {
-  button.addEventListener('click', function() {
+  button.addEventListener('click', function(evt) {
+    evt.preventDefault();
     overlay.classList.remove('show');
     shoppingModal.classList.remove('show');
     checkoutModal.classList.remove('show');
+    thanksModal.classList.remove('show');
   });
+});
+
+overlay.addEventListener('click', function(evt) {
+  overlay.classList.remove('show');
+  shoppingModal.classList.remove('show');
+  checkoutModal.classList.remove('show');
+  thanksModal.classList.remove('show');
 });
 
 window.addEventListener('keydown', function(evt) {
@@ -29,12 +60,7 @@ window.addEventListener('keydown', function(evt) {
       overlay.classList.remove('show');
       shoppingModal.classList.remove('show');
       checkoutModal.classList.remove('show');
+      thanksModal.classList.remove('show');
     }
   }
-});
-
-checkoutButton.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  shoppingModal.classList.remove('show');
-  checkoutModal.classList.add('show');
 });
